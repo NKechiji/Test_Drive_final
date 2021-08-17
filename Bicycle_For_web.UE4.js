26ab50c5256b8309dcfd0833b8d85fce1ec2d291
NKechiji <89080286+NKechiji@users.noreply.github.com>
2021-08-17 20:32:12 +0600

    Update Bicycle_For_web.UE4.js

--- Bicycle_For_web.UE4.js
+++ Bicycle_For_web.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Bicycle_For_web.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/NKechiji/Test_Drive_final/624c6d947d0136c113a93aec48043be46544c973/Bicycle_For_web.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
